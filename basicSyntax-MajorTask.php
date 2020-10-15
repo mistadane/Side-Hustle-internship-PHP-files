d3d6dabc8d5303849b2b8ff23939db0d1d68c202
mistadane <samrehonline03@gmail.com>
2020-10-15 16:38:00 -0500

    Side Hustle first major task

---
+++ basicSyntax-MajorTask.php
@@ -0,0 +1,25 @@
+<?php 
+// ====== First Major task ====
+
+/*
+Occupation: Freelance Web Developer
+Gender: Male
+Sidehustle Username: samreh
+ */
+
+
+// About me tag
+echo "<h1>About Me</h1> <hr/>";
+
+// Sidehustle Full name and username
+echo "<h3>Samir Areh</h3>";
+echo "<h4>samreh</h4>";
+
+// Brief details about me
+echo "<p>
+        Technically proficient, business savvy leader and management professional. 
+        Skilled in planning and maintaining work systems, procedures, and policies that enable and encourage 
+        the optimum performance of its people and other resources within a business unit.
+
+     </p>"
+?>
